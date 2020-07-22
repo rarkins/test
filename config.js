fc6f46f08a6b0291c6f9e097e983ad944a36483f
Maerov Timofey Pavlovich <t.p.maerov@tinkoff.ru>
2020-07-22 14:47:53 +0300

    init

--- config.js
+++ config.js
@@ -1,14 +1,16 @@
 module.exports = {
-    endpoint: 'https://github.com/',
-    username: 'Monax111',
-    password: '',
-    gitAuthor: 'RenovateBot',
+    token: '***',
+    gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
     platform: 'github',
     logFileLevel: 'debug',
     logLevel: 'debug',
     onboarding: true,
     onboardingConfig: {
-        extends: ['config:base']
+        extends: ['@bpm-renovate:base-backend-kotlin']
     },
-    repositories: ['Monax111/test']
+    repositories: ['Monax111/test'],
+	encrypted: {
+        npmToken: "***"
+    },
+    npmrc: "registry=https://nexus-new.tcsbank.ru/repository/npm-all/\nstrict-ssl=false\nca=''\n//nexus-new.tcsbank.ru/repository/npm-sme/:_authToken=${NPM_TOKEN}"
 };
