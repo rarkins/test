2f4c94a708d62022a3dc304b07419650fd04b240
Maerov Timofey Pavlovich <t.p.maerov@tinkoff.ru>
2020-07-22 18:50:26 +0300

    init

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: '***',
+    token: '040e372a91be8a2d20891286d4a25dd826536dce',
     gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
     platform: 'github',
     logFileLevel: 'debug',
