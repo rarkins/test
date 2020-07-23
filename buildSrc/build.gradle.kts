be7b3cf350df922eaffa165c291895f79e7ed1a9
Maerov Timofey Pavlovich <t.p.maerov@tinkoff.ru>
2020-07-23 12:34:21 +0300

    alternative plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("org.springframework.boot") version "2.2.4.RELEASE" apply false
+    //id("org.springframework.boot") version "2.2.4.RELEASE" apply false
     kotlin("jvm") version "1.3.60"
 }
 
@@ -7,6 +7,15 @@ repositories{
     jcenter()
 }
 
-dependencies {
-    //implementation("org.springframework.boot:spring-boot-gradle-plugin:2.2.4.RELEASE")
+id("org.springframework.boot") version "2.2.4.RELEASE"
+
+class Version(val name:String){
+    lateinit var version:String
 }
+
+fun id(name: String):Version{
+    println(name)
+    return Version(name)
+}
+
+infix fun Version.version(version :String) { this.version = version}
